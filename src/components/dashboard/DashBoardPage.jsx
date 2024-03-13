7d1eaf6c35e3ac6bd3b7bdc36cfc4dc1386bdc26
adithyan <adithyanpradeep@36>
2024-03-13 10:34:45 +0530

    commit

---
+++ src/components/dashboard/DashBoardPage.jsx
@@ -0,0 +1,56 @@
+import { Link } from 'react-router-dom';
+import './Dashboard.css'
+
+export default function DashBoardPage() {
+    return (
+        <div className='app-container'>
+          <div className='img-nav'>
+    
+            
+            <div className='tile'>
+              <Link to={'/add_user'}>
+              <div className='content'>
+                <h1>ADD USER</h1>
+                
+              </div></Link>
+             
+            </div>
+            
+            
+    
+            {/* Tile 2 */}
+            <div className='tile'>
+            <Link to={'/'}>
+              <div className='content'>
+                <h1>HOME</h1>
+                
+              </div></Link>
+              
+             
+            </div>
+    
+            {/* Tile 3 */}
+            
+            <div className='tile'>
+              <Link to={'/viewUser'}>
+              <div className='content'>
+                <h1>USER LIST</h1>
+              
+              </div>
+              </Link>
+            </div>
+    
+            {/* Tile 4 */}
+            <div className='tile'>
+              
+              <div className='content'>
+                <h1>LOGOUT</h1>
+                
+              </div>
+              <div className='img'></div>
+            </div>
+    
+          </div>
+        </div>
+      );
+}
---
+++ src/components/landing/Landing.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import {BrowserRouter as Router, Routes, Route, Link}from "react-router-dom";
+// import LandingLogin from "../login/LandingLogin";
+import Login from "../login/Login";
+// import Landing_Nav from "../landing/Landing_Nav";
+import Landing_Page from "./Landing_Page";
+import AddUser from "../add_user/AddUser";
+import Dashboard from "../dashboard/DashBoard";
+// import Display from "../display/Display";
+import UserList from "../users_list/UserList";
+
+function Landing() {
+    return(
+
+<>
+       
+       <Router>
+        <>
+        <Routes>
+            {/* <Route path={"/"} exact element={<Landing_Nav/>}/> */}
+            <Route path={"/"} exact element={<Landing_Page/>}/>
+            <Route path="/login" exact element={<Login/>}/>
+            <Route path="/admin" exact element={<Dashboard/>}/>
+            <Route path="/add_user" exact element={<AddUser/>}/>
+            <Route path="/home" exact element = {<Landing_Page/>}/>
+            <Route path="viewUser" exact element = {<UserList/>}/>
+        </Routes>
+        </>
+       </Router> 
+       </>
+    )
+}
+export default Landing ;
